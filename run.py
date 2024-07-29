80e131ebd104cd341c01c98daa33ea4c29bea5d5
HenilMistry <henilmistry74496@gmail.com>
2024-07-29 14:12:08 +0530

    Added a functionality for the bot to be able to run from CLI

--- run.py
+++ run.py
@@ -1,12 +1,27 @@
 from booking.booking import Booking
 
-with Booking() as bot:
-    bot.land_first_page()
-    bot.check_for_signin_info()
-    bot.change_currency("INR")
-    bot.select_place_to_go("Surat")
-    bot.select_dates(check_in_date="2024-08-01", check_out_date="2024-08-05")
-    bot.select_adults(2)
-    bot.click_search()
-    bot.apply_alterations()
-    print("Exiting...")
+try:
+    with Booking() as bot:
+        bot.land_first_page()
+        bot.check_for_signin_info()
+        bot.change_currency("INR")
+        bot.select_place_to_go("Surat")
+        bot.select_dates(check_in_date="2024-08-01", check_out_date="2024-08-05")
+        bot.select_adults(2)
+        bot.click_search()
+        bot.apply_alterations()
+        print("Exiting...")
+
+except Exception as e:
+    if 'in PATH' in str(e):
+        print("""
+            You are trying to run the bot from the command line
+            Please add to PATH your Selenium Drivers
+            Windows:
+                set PATH=%PATH%;<path-to-your-driver>
+                
+            Linux:
+                PATH=$PATH:/<path-to-your-folder>
+        """)
+    else:
+        raise
